8e37e659972285f4196f8b2c9bfecdc69a9d993b
tonytan4ever <tonytan198211@gmail.com>
2015-04-08 23:07:51 -0400

    Add index page

--- shell/styles/controllers/main.py
+++ shell/styles/controllers/main.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template, flash, request, redirect, url_for
-from flask.ext.login import login_user, logout_user, login_required
 
 from shell.styles.extensions import cache
 #from appname.forms import LoginForm
@@ -11,4 +10,4 @@ main = Blueprint('main', __name__)
 @main.route('/')
 @cache.cached(timeout=1000)
 def home():
-    return "Hello World!"
+    return render_template('main/index.html')
