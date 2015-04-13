4bb98b1114f9a6c4cf66b51ef0e7875dfcb3cbab
tonytan4ever <tonytan198211@gmail.com>
2015-04-13 18:01:57 -0400

    More fix on nav and 401 pages

--- shell/styles/controllers/main.py
+++ shell/styles/controllers/main.py
@@ -1,8 +1,6 @@
 from flask import Blueprint, render_template, flash, request, redirect, url_for
 
 from shell.styles.extensions import cache
-#from appname.forms import LoginForm
-#from appname.models import User
 
 main = Blueprint('main', __name__)
 
